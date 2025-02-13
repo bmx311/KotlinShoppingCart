2f25e188a1044e19964c3fff36387db40a048049
abdulwahab alsenan <ak-alsenan@hotmail.com>
2025-02-13 20:26:17 +0300

    partially completed

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,25 @@
 package shoppingCart
 
+import kotlin.collections.MutableMap as MutableMap1
+
 /**
- * Returns a map of productId -> MutableMap("name" to String, "price" to Double, "stock" to Int).
- * Your task: Add exactly 5 items with correct details (IDs 1..5).
- * Example:
- *   1 -> { "name"="Laptop", "price"=350.000, "stock"=10 },
- *   2 -> { "name"="Smart TV", "price"=200.000, "stock"=5 },
- *   3 -> { "name"="Headphones", "price"=50.000, "stock"=20 },
- *   4 -> { "name"="Gaming Console", "price"=150.000, "stock"=8 },
- *   5 -> { "name"="Wireless Mouse", "price"=8.500, "stock"=15 },
- */
-fun createStoreInventory(): MutableMap<Int, MutableMap<String, Any>> {
-    TODO("Implement createStoreInventory()")
+  Returns a map of productId -> MutableMap("name" to String, "price" to Double, "stock" to Int).
+  Your task: Add exactly 5 items with correct details (IDs 1..5).
+  Example:
+  //    1 -> { "key1"="Laptop", "price"=350.000, "stock"=10 },
+  //    2 -> { "key2"="Smart TV", "price"=200.000, "stock"=5 },
+  //    3 -> { "key3"="Headphones", "price"=50.000, "stock"=20 },
+  //    4 -> { "key4"="Gaming Console", "price"=150.000, "stock"=8 },
+  //    5 -> { "key5"="Wireless Mouse", "price"=8.500, "stock"=15 },
+**/
+fun createStoreInventory(): MutableMap1<Int, MutableMap1<String, Any>> {
+    return mutableMapOf(
+        1 to mutableMapOf("key1" to "Laptop", "price1" to 350.00, "stock" to 10),
+        2 to mutableMapOf("key2" to "Smart TV", "price2" to 200.00, "stock" to 5),
+        3 to mutableMapOf("key3" to "Headphones", "price3" to 50.00, "stock" to 20),
+        4 to mutableMapOf("key4" to "Gaming Console", "price4" to 150.00, "stock" to 8),
+        5 to mutableMapOf("key5" to "Wireless Mouse", "price5" to 8.500, "stock" to 15)
+    )
 }
 
 /**
@@ -20,12 +28,31 @@ fun createStoreInventory(): MutableMap<Int, MutableMap<String, Any>> {
  * - Return true if successful, false otherwise.
  */
 fun addToCart(
-    storeInventory: MutableMap<Int, MutableMap<String, Any>>,
-    cart: MutableMap<Int, Int>,
+    storeInventory: MutableMap1<Int, MutableMap1<String, Any>>,
+    cart: MutableMap1<Int, Int>,
     productId: Int,
     quantity: Int
 ): Boolean {
-    TODO("Implement addToCart()")
+        var productStock = storeInventory[productId]?.get("stock") as Int
+
+        if (productStock.toString().toInt() >= quantity) {
+            if(cart.get(productId) != null){
+                var oldQuan = cart.get((productId)) as Int;
+
+                cart.put(productId, quantity +oldQuan );
+            }else{
+                cart.put(productId, quantity)
+            }
+
+            storeInventory.get(productId)?.set("stock", productStock - quantity)
+
+
+            return true
+        } else {
+            return false
+
+        }
+
 }
 
 /**
@@ -34,24 +61,37 @@ fun addToCart(
  * - Return true if successful, false otherwise.
  */
 fun removeFromCart(
-    storeInventory: MutableMap<Int, MutableMap<String, Any>>,
-    cart: MutableMap<Int, Int>,
+    storeInventory: MutableMap1<Int, MutableMap1<String, Any>>,
+    cart: MutableMap1<Int, Int>,
     productId: Int,
     quantity: Int
 ): Boolean {
-    TODO("Implement removeFromCart()")
-}
+    var productStock = storeInventory[productId]?.get("stock") as Int
 
+    if (cart.get(productId) != null) {
+        cart.put(productId, cart.get(productId).toString().toInt() - quantity)
+        storeInventory.get(productId)?.set("stock", productStock + quantity)
+        return true
+    } else {
+        return false
+    }
+}
 /**
  * Calculates the total cost of items in [cart].
  * - For each (productId -> quantity), retrieve the price from [storeInventory].
  * - Multiply (price * quantity) and sum for the final total.
  */
 fun calculateTotal(
-    storeInventory: MutableMap<Int, MutableMap<String, Any>>,
-    cart: MutableMap<Int, Int>
+    storeInventory: MutableMap1<Int, MutableMap1<String, Any>>,
+    cart: MutableMap1<Int, Int>
 ): Double {
-    TODO("Implement calculateTotal()")
+    if (cart.get(productId) != null) {
+
+
+        
+    }
+    else
+        return 0.0
 }
 
 /**
@@ -60,7 +100,7 @@ fun calculateTotal(
  * - Matching is case-insensitive.
  */
 fun filterProductsByName(
-    storeInventory: MutableMap<Int, MutableMap<String, Any>>,
+    storeInventory: MutableMap1<Int, MutableMap1<String, Any>>,
     keyword: String
 ): List<Int> {
     TODO("Implement filterProductsByName()")
